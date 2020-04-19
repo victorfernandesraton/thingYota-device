ab5da779fd33fe45132f67effd44bd7a10e03dbf
victorfernandesraton <vfbraton@gmail.com>
2020-04-19 12:24:10 -0300

    implementing: get method with works

--- lib/http/HttpRequest.cpp
+++ lib/http/HttpRequest.cpp
@@ -6,40 +6,221 @@
 
 using namespace std;
 
-
-HttpRequest::HttpRequest(WiFiClient wifiClient, String host, u16_t port, String uri) {
-    _wifiClient = wifiClient;
+HttpRequest::HttpRequest(String host, u16_t port, String uri)
+{
     _baseUrl = uri;
-    _host= host;
-    _port= port;
+    _host = host;
+    _port = port;
     _headers = "";
 }
 
-HttpRequest::HttpRequest(WiFiClient wifiClient, String host, u16_t port, String uri, String headers) {
-    _wifiClient = wifiClient;
+HttpRequest::HttpRequest(String host, u16_t port, String uri, String headers)
+{
     _baseUrl = uri;
-    _host= host;
-    _port= port;
+    _host = host;
+    _port = port;
     _headers = headers;
 }
 
-HttpResponse HttpRequest::get(String url) {
+HttpResponse HttpRequest::get(String url)
+{
     HTTPClient http;
     HttpResponse httpResult;
-    if (http.begin(_host, _port, _baseUrl)) {
-        if (http.GET()) {
+    Serial.println("REQUEST :[GET] ::" +_host+_port+_baseUrl+url);
+    http.begin(_host+":"+_port+_baseUrl+url);
+    if (http.GET() > 0)
+    {
+        
+        httpResult.responseCode = http.GET();
+        httpResult.response = http.getString();
+        httpResult.responseError = "";
+    }
+    else
+    {
+        httpResult.response = "Error: Server internal Errror";
+        httpResult.responseCode = 500;
+        httpResult.responseError = http.errorToString(httpResult.responseCode).c_str();
+    }
+    http.end();
+    return httpResult;
+}
+
+HttpResponse HttpRequest::get(String url, String query)
+{
+   
+    HTTPClient http;
+    HttpResponse httpResult;
+    Serial.println("REQUEST :[GET] ::" +_host+_port+_baseUrl+url+query);
+    http.begin(_host+":"+_port+_baseUrl+url+query);
+    if (http.GET() > 0)
+    {
+        
+        httpResult.responseCode = http.GET();
+        httpResult.response = http.getString();
+        httpResult.responseError = "";
+    }
+    else
+    {
+        httpResult.response = "Error: Server internal Errror";
+        httpResult.responseCode = 500;
+        httpResult.responseError = http.errorToString(httpResult.responseCode).c_str();
+    }
+    http.end();
+    return httpResult;
+}
+
+HttpResponse HttpRequest::get(String url, String query, String parans)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+    if (http.begin(_host, _port, _baseUrl + query + parans))
+    {
+        if (http.GET())
+        {
             httpResult.responseCode = http.GET();
             httpResult.response = http.getString();
             httpResult.responseError = "";
-         } else {
+        }
+        else
+        {
             httpResult.response = "Error: Server internal Errror";
             httpResult.responseCode = 500;
-            httpResult.responseError = http.errorToString(httpResult.responseCode).c_str();   
-         } 
-    } else {
+            httpResult.responseError = http.errorToString(httpResult.responseCode).c_str();
+        }
+    }
+    else
+    {
         httpResult.response = "Error: Not Found";
         httpResult.responseCode = 400;
         httpResult.responseError = "";
     }
+    http.end();
+    return httpResult;
+}
+
+HttpResponse HttpRequest::post(String url)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+
+    return httpResult;
+}
+HttpResponse HttpRequest::post(String url, String query)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+HttpResponse HttpRequest::post(String url, String query, String parans)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+HttpResponse HttpRequest::post(String url, String query, String parans, String body)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+
+// Métodos put
+HttpResponse HttpRequest::put(String url)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+HttpResponse HttpRequest::put(String url, String query)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+HttpResponse HttpRequest::put(String url, String query, String parans)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+HttpResponse HttpRequest::put(String url, String query, String parans, String body)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+
+// Métodos delete
+HttpResponse HttpRequest::del(String url)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+HttpResponse HttpRequest::del(String url, String query)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
+    return httpResult;
+}
+HttpResponse HttpRequest::del(String url, String query, String parans)
+{
+    HTTPClient http;
+    HttpResponse httpResult;
+
+    Serial.println("Not Implemented");
+    httpResult.response = "Error: Not Found";
+    httpResult.responseCode = 400;
+    httpResult.responseError = "";
     return httpResult;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
 #include <SocketIoClient.h>
-
+#include "HttpRequest.hpp"
 #define Serial Serial
 
 ESP8266WiFiMulti WiFiMulti;
@@ -11,51 +11,64 @@ SocketIoClient webSocket;
 bool connect = false;
 
 // eventos
-void event(const char * payload, size_t length) {
-  Serial.printf("got message: %s\n", payload);
+void event(const char *payload, size_t length)
+{
+    Serial.printf("got message: %s\n", payload);
 }
-void teste(const char * payload, size_t length) {
-  Serial.println("Teste %s");
-  Serial.printf("%s\n",payload);
-  // emiter
-  webSocket.emit("ok","ok");
+void teste(const char *payload, size_t length)
+{
+    Serial.println("Teste %s");
+    Serial.printf("%s\n", payload);
+    // emiter
+    webSocket.emit("ok", "ok");
 }
 
-void connectHandler(const char * payload, size_t length) {
-	connect = true;
+void connectHandler(const char *payload, size_t length)
+{
+    connect = true;
 }
-void setup() {
+void setup()
+{
     Serial.begin(9600);
 
     Serial.println();
     Serial.println();
     Serial.println();
 
-      for(uint8_t t = 4; t > 0; t--) {
-          Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
-          Serial.flush();
-          Serial.println(WiFi.macAddress);
-          delay(1000);
-      }
+    WiFi.begin("Maria Clara", "11010000");
+    for (uint8_t t = 4; t > 0; t--)
+    {
+        Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+        Serial.flush();
+        // Serial.println(WiFi.macAddress);
+        delay(1000);
+    }
 
     WiFiMulti.addAP("Maria Clara", "11010000");
 
-    while(WiFiMulti.run() != WL_CONNECTED) {
+    while (WiFiMulti.run() != WL_CONNECTED)
+    {
         delay(100);
     }
 
     webSocket.on("event", event);
-	webSocket.on("teste", teste);
-	webSocket.on("connect", connectHandler);
+    webSocket.on("teste", teste);
+    webSocket.on("connect", connectHandler);
 
-    webSocket.begin("123.23.23.20",4000);
+    webSocket.begin("123.23.23.20", 4000);
     // use HTTP Basic Authorization this is optional remove if not needed
     // webSocket.setAuthorization("username", "password");
 }
 
-void loop() {
-    webSocket.loop();
-	if (connect) {
-		Serial.println("Conectado");
-	}
+void loop()
+{
+    HttpRequest *custonHttp = new HttpRequest("http://123.23.23.20", 8000, "/helth");
+    Serial.println(custonHttp->get("/ok").response); 
+    Serial.println(custonHttp->get("/ok").responseCode); 
+    Serial.println(custonHttp->get("/ok").responseError); 
+    // webSocket.loop();
+    // if (connect)
+    // {
+    //     Serial.println("Conectado");
+    // }
 }
